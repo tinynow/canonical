7526b4fc343e2f331214290a2dda926a25380aa2
tinynow <matt@tinynow.com>
2020-06-19 21:47:00 -0700

    kiss

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -64,14 +64,13 @@
             </router-link>
         </nav>
     </header>
-    <h1>A playground for serious play</h1>
-    <div class="canon-layout --readable --auto-flow mt3">
-        <p>I am a multi-disciplinary person who works full time for a large state agency. There, I do web development and design with a focus on accessibility, design systems, and content strategy. Here, I am free to sweat the small stuff and revel in the green fields.</p>
-        <p>
-            This site will change frequently until it doesn't. For now,  <router-link to="/palette">
+    <div class="canon-layout --tube pt3">
+        <h1>A playground for serious play</h1>
+        <div class="canon-layout --readable --auto-flow mt3">
+            <router-link to="/palette">
                 Try this Color Combinations tool
             </router-link>. More to come.
-        </p>
+        </div>
     </div>
 </div>
 </template>
--- src/stories/2-Typography.stories.js
+++
@@ -1,8 +0,0 @@
-import CanonTypography from './Typography.vue'
-
-export default {
-    title: 'Typography',
-    component: CanonTypography,
-}
-export const defaultElements = () => '<canon-typography/>';
-export const kitchenSinkAutoFlow = () => '<canon-typography class="canon-layout --auto-flow"/>';
