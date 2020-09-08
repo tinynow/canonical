bd5bde071e9ef17863e9b3a4b1e8ce683ae2088c
tinynow <matt@tinynow.com>
2020-09-08 14:40:04 -0700

    rename field

--- src/components/Palette/Palette.vue
+++ src/components/Palette/Palette.vue
@@ -144,7 +144,7 @@ import CanonIcon from '../Icon/Icon';
 import CanonCheckbox from '../Checkbox/Checkbox';
 import CanonButton from '../Button/Button';
 import CanonForm from '../Form/Form';
-import CanonField from '../InputField/InputField';
+import CanonField from '../Field/Field';
 
 const a11yLevelOptions = [
     {
