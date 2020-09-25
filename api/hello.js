4cbd406624bfc25efe5125df41a0f37cd4791d16
tinynow <matt@tinynow.com>
2020-09-25 15:56:13 -0700

    test

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,4 @@
 exports.handler = function (event, context, callback) {
-    const { SPOTIFY_SECRET } = process.env
     callback(null, {
         statusCode: 200,
         body: 'Hello, World' + SPOTIFY_SECRET,
