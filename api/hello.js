e91787784dea6223f3600654148dcf88c7e89236
tinynow <matt@tinynow.com>
2020-09-25 15:50:27 -0700

    test to get environment vars

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,7 @@
 exports.handler = function (event, context, callback) {
+    const { SPOTIFY_SECRET } = process.env
     callback(null, {
         statusCode: 200,
-        body: 'Hello, World' + process.env,
+        body: 'Hello, World' + SPOTIFY_SECRET,
     });
 }
