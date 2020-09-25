495b70a0fedc297c63201404719af465a83355a6
tinynow <matt@tinynow.com>
2020-09-25 16:12:02 -0700

    spotify test1

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,18 @@
-exports.handler = function (event, context, callback) {
-    const { SPOTIFY_SECRET } = process.env;
-    callback(null, {
-        statusCode: 200,
-        body: 'Hello, World' + SPOTIFY_SECRET,
-    });
-}
+import fetch from 'node-fetch';
+const { SPOTIFY_SECRET } = process.env;
+const API_ENDPOINT = 'https://api.spotify.com/v1/users/tinynow/playlists';
+
+exports.handler = async (event, context) => {
+    return fetch(API_ENDPOINT, {
+        headers: {
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer BQD1fNL3vAdPwVMOg3XzoEhHPuu0IL0iDq2iLc-XyTNKF2j5XSHaUkxXP7HRG1dSFUxyxTQ_tbA7uWkmNd_3mSQsH03BWTp0i0sG_wsN1AUeds05q8jLckAdDw9O57PADEhzIJmL1N3oJeB0006IfqNaMhk',
+         } })
+        .then(response => response.json())
+        .then(data => ({
+            statusCode: 200,
+            body: data,
+        }))
+        .catch(error => ({ statusCode: 422, body: String(error) }));
+};
