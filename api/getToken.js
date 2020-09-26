434ceaec57bfbaf84541e9393d6194c4a174a966
tinynow <matt@tinynow.com>
2020-09-26 15:46:38 -0700

    victory - can get a playlist now!

--- api/getToken.js
+++ api/getToken.js
@@ -6,7 +6,7 @@ const encodeToB64 = string => {
 
 const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env;
 const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
-// const PLAYLIST_ENDPOINT = 'https://api.spotify.com/v1/users/tinynow/playlists';
+const PLAYLIST_ENDPOINT = 'https://api.spotify.com/v1/users/tinynow/playlists';
 
 const credentials = `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`;
 const basicAuth = `Basic ${encodeToB64(credentials)}`
@@ -19,15 +19,32 @@ const urlencoded = new URLSearchParams();
 urlencoded.append('grant_type', 'client_credentials');
 const tokenRequestOptions = {
     method: 'POST',
-    headers: myHeaders,
+    headers: headers,
     body: urlencoded,
     redirect: 'follow',
 };
 
 
+const fetchDataFromSpotify = async result => { 
+    // "access_token": "BQDPYwJsh..GMEdA", "token_type": "Bearer", "expires_in": 3600
+    const { access_token, token_type } = JSON.parse(result);
+    const auth = `${token_type} ${access_token}`
+    const headers = {
+       'Authorization': auth,
+    }
+    const requestOptions = {
+        method: 'GET',
+        headers: headers,
+    };
+    //TODO: abstract for other endpoints
+    return fetch(PLAYLIST_ENDPOINT, requestOptions)
+        .then(response => response.text());
+}
+
 exports.handler = async (event, context) => {
     return fetch(TOKEN_ENDPOINT, tokenRequestOptions)
         .then(response => response.text())
+        .then(result => fetchDataFromSpotify(result))
         .then(result => ({
             statusCode: 200,
             body: result,
