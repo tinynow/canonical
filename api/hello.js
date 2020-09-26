5cb4e50952ba022e10e8be23c44a31e52da99b7a
tinynow <matt@tinynow.com>
2020-09-25 17:23:57 -0700

    add spotify function

--- api/hello.js
+++ api/hello.js
@@ -1,33 +1,19 @@
 const fetch = require('node-fetch');
 
-const API_ENDPOINT = 'https://icanhazdadjoke.com/';
+const { SPOTIFY_SECRET } = process.env;
+const API_ENDPOINT = 'https://api.spotify.com/v1/users/tinynow/playlists';
 
 exports.handler = async (event, context) => {
-    return fetch(API_ENDPOINT, { headers: { 'Accept': 'application/json' } })
+    return fetch(API_ENDPOINT, {
+        headers: {
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer BQD1fNL3vAdPwVMOg3XzoEhHPuu0IL0iDq2iLc-XyTNKF2j5XSHaUkxXP7HRG1dSFUxyxTQ_tbA7uWkmNd_3mSQsH03BWTp0i0sG_wsN1AUeds05q8jLckAdDw9O57PADEhzIJmL1N3oJeB0006IfqNaMhk',
+         } })
         .then(response => response.json())
         .then(data => ({
             statusCode: 200,
-            body: data.joke,
+            body: data,
         }))
         .catch(error => ({ statusCode: 422, body: String(error) }));
 };
-
-
-// import fetch from 'node-fetch';
-// const { SPOTIFY_SECRET } = process.env;
-// const API_ENDPOINT = 'https://api.spotify.com/v1/users/tinynow/playlists';
-
-// exports.handler = async (event, context) => {
-//     return fetch(API_ENDPOINT, {
-//         headers: {
-//             'Accept': 'application/json', 
-//             'Content-Type': 'application/json',
-//             'Authorization': 'Bearer BQD1fNL3vAdPwVMOg3XzoEhHPuu0IL0iDq2iLc-XyTNKF2j5XSHaUkxXP7HRG1dSFUxyxTQ_tbA7uWkmNd_3mSQsH03BWTp0i0sG_wsN1AUeds05q8jLckAdDw9O57PADEhzIJmL1N3oJeB0006IfqNaMhk',
-//          } })
-//         .then(response => response.json())
-//         .then(data => ({
-//             statusCode: 200,
-//             body: data,
-//         }))
-//         .catch(error => ({ statusCode: 422, body: String(error) }));
-// };
