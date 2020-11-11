e410668376e2b0f0bf82ab8f6d53b858e1afa7b5
tinynow <matt@tinynow.com>
2020-11-10 19:10:53 -0800

    style tweaks

--- src/components/Playlist/Welcome.vue
+++ src/components/Playlist/Welcome.vue
@@ -1,20 +1,20 @@
 <template>
-<div class="canon-layout --tube --auto-flow tc pt3">
-    <h2 class="fw800">
-        Your playlists were born from a backstory, not an algorithm.
-    </h2>
-    <div>
-        <p class="canon-u-type--2">
+<div class="flex flex-column">
+    <div class="canon-layout --tube tc pt3">
+        <h2 class="fw800">
+            Your playlists were born from a backstory, not an algorithm.
+        </h2>
+
+        <p class="canon-u-type--2 mt4">
             Make them personal again.
         </p>
-        <p class="canon-u-type--2">
+        <p class="canon-u-type--2 mt2">
             Import Spotify playlists or curate a new mix. Add notes. Share.
         </p>
-    </div>
     
-    <div class="flex items-center justify-center pv3">
+ 
         <button
-            class="canon-button canon-u-type--3"
+            class="canon-button canon-u-type--3 dib mt4"
             type="button"
             aria-describedby="disclaimer"
             @click="createAccountClick"
@@ -22,12 +22,11 @@
             Get Early Access*
         </button>
     </div>
-
     <p
         id="disclaimer"
-        class="pa1 bg--dark text--light"
+        class="pa1 bg--dark text--light mt5"
     >
-        *This app is in beta! I will do my best to avoid losing anything you save here, but please proceed with caution.
+        *This app is in alpha! I will do my best to avoid losing anything you save here, but please proceed with caution.
     </p>
 </div>
 </template>
